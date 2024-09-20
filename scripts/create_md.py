cdc2a3da32ce52f27312e7408de6c817e25421c8
kevin <ye@126.com>
2024-09-21 02:48:52 +0800

    add

--- scripts/create_md.py
+++ scripts/create_md.py
@@ -1,3 +1,6 @@
+'''
+Description: Test
+'''
 import os
 import mysql.connector
 import pandas as pd
@@ -41,9 +44,13 @@ def get_posts_data(connection):
 # 生成Markdown表格
 def generate_markdown_tables(posts_data,bj_time,day,beijing_tz):
     df = pd.DataFrame(posts_data, columns=["title", "description", "category", "link", "date"])
+    row_count = df.shape[0]
+    time_obj = datetime.fromisoformat(str(bj_time))
+    formatted_time = time_obj.strftime("%Y-%m-%d %H:%M:%S")
 
     # 按分类分组
     grouped = df.groupby("category")
+    markdown_header = f"# 出海工具箱\n\n> 本文档更新时间:{formatted_time}，欢迎star。[**新增**] 表示7天内增加的。\n\n[出海工具箱](http://www.chgjx.com)收集各类建站、SEO、选品、营销推广等工具，目前已收集**{row_count}**种服务/网站/工具，为广大独立开发者出海，跨境电商从业者，自媒体从业者服务，助力您取得成功！\n\n"
 
     markdown_tables = []
 
@@ -59,30 +66,27 @@ def generate_markdown_tables(posts_data,bj_time,day,beijing_tz):
         markdown_table += df_selected.to_markdown(index=False)
         markdown_tables.append(markdown_table)
 
-    return "\n\n".join(markdown_tables)
+    return markdown_header + "\n\n".join(markdown_tables)
 
 # 将Markdown内容写入文件
-def write_to_markdown_file(markdown_content, beijing_time, filename="README.md"):
+def write_to_markdown_file(markdown_content, filename="README.md"):
     with open(filename, "w", encoding="utf-8") as file:
-        markdown_header = f"# 出海工具箱\n\n> 本文档更新时间:{beijing_time}，欢迎star。[**新增**] 表示7天内增加的。\n\n[出海工具箱](http://www.chgjx.com)收集各类建站、SEO、选品、营销推广等工具，为广大独立开发者出海，跨境电商从业者，自媒体从业者服务，助力您取得成功！\n\n"
-        file.write(markdown_header)
+        # file.write(markdown_header)
         file.write(markdown_content)
 
 # 主函数
 def main():
-        # 获取当前的UTC时间
+    # 获取当前的UTC时间
     utc_now = datetime.now(pytz.utc)
-
     # 将UTC时间转换为北京时间
     beijing_tz = pytz.timezone('Asia/Shanghai')
     beijing_time = utc_now.astimezone(beijing_tz)
-    time_obj = datetime.fromisoformat(str(beijing_time))
-    formatted_time = time_obj.strftime("%Y-%m-%d %H:%M:%S")
+
 
     connection = connect_to_database()
     posts_data = get_posts_data(connection)
     markdown_content = generate_markdown_tables(posts_data,beijing_time,7,beijing_tz)
-    write_to_markdown_file(markdown_content,formatted_time)
+    write_to_markdown_file(markdown_content)
     connection.close()
 
 if __name__ == "__main__":
